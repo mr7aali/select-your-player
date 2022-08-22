9995cdfb65d8c0bda37cd09e354341edf052cc51
mr7aali <mr7aali@gmail.com>
2022-08-23 00:56:52 +0600

    selector added

---
+++ Js/javaScript.js
@@ -0,0 +1,25 @@
+let len = document.querySelectorAll('.btn-1').length;
+
+const selectedName=[];
+let tem=0;
+for(let i =0; i<len; i++){
+    if(tem===5){
+        break;
+    }
+    document.querySelectorAll('.btn-1')[i].addEventListener('click',function(){
+      const getNameElement = document.querySelectorAll('.player-name')[i];
+      const getName = getNameElement.innerText;
+      selectedName.push(getName);
+
+    listNode = document.getElementById('list');
+    liNode = document.createElement("LI");
+    textNode = document.createTextNode(getName);
+
+    liNode.appendChild(textNode);
+    listNode.appendChild(liNode);
+    
+     tem++;
+    });
+  
+}                                                   
+
