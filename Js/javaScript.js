36bc39c3aa982939899b6612f38087daf4541c5b
mr7aali <mr7aali@gmail.com>
2022-08-23 07:22:09 +0600

    remove js code

--- Js/javaScript.js
+++ Js/javaScript.js
@@ -1,25 +1,36 @@
-let len = document.querySelectorAll('.btn-1').length;
+// let len = document.querySelectorAll('.btn-1').length;
 
-const selectedName=[];
-let tem=0;
-for(let i =0; i<len; i++){
-    if(tem===5){
-        break;
-    }
-    document.querySelectorAll('.btn-1')[i].addEventListener('click',function(){
-      const getNameElement = document.querySelectorAll('.player-name')[i];
-      const getName = getNameElement.innerText;
-      selectedName.push(getName);
+// const selectedName=[];
+// let tem=0;
 
-    listNode = document.getElementById('list');
-    liNode = document.createElement("LI");
-    textNode = document.createTextNode(getName);
+//     for(let i =0; i<len; i++){
+//         document.querySelectorAll('.btn-1')[i].addEventListener('click',function(){
+//             const getNameElement = document.querySelectorAll('.player-name')[i];
+//             const getName = getNameElement.innerText;
+//             selectedName.push(getName);
+      
+//       let ali=i;
+      
+//       console.log(ali);
 
-    liNode.appendChild(textNode);
-    listNode.appendChild(liNode);
-    
-     tem++;
-    });
+
+//           listNode = document.getElementById('list');
+//           liNode = document.createElement("LI");
+//           textNode = document.createTextNode(getName);
+//           liNode.appendChild(textNode);
+//           listNode.appendChild(liNode); 
+          
+          
+
+//           });
+         
+//        }
+
+
+
+
+
+
   
-}                                                   
+                                                 
 
